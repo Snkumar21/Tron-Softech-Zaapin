4cca55376c23d8454f1a4f155c87a956a3f6e31a
Nitish <snkumar6926@gmail.com>
2025-06-23 15:22:32 +0530

    add new products fully updated

--- src/components/pages/Products/AddNewProduct.jsx
+++ src/components/pages/Products/AddNewProduct.jsx
@@ -1,19 +1,242 @@
-import React from 'react';
-import { Box, Typography, Paper } from '@mui/material';
+import React, { useState } from 'react';
+import {
+    Box, Typography, Paper, TextField, MenuItem, Button, Grid
+} from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
-function AddNewProduct() {
+const categories = ['-- Select Category --', 'Food', 'Beverage'];
+const subCategories = ['-- Select Sub-Category --', 'Snacks', 'Juice'];
+const productTypes = ['VEG', 'NON-VEG'];
+const offers = ['-- No Offer --', '10% Off', 'Buy 1 Get 1'];
+const recommended = ['Yes', 'No'];
+
+const AddNewProduct = () => {
+    const [form, setForm] = useState({
+        category: '',
+        subCategory: '',
+        productType: '',
+        productName: '',
+        productCode: '',
+        strikePrice: '',
+        displayPrice: '',
+        availableOutlet: '',
+        productSequence: '',
+        productDescription: '',
+        offer: '',
+        recommended: '',
+        link: ''
+    });
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(form);
+        // submit logic here
+    };
+
     return (
         <Box sx={{ flexGrow: 1, p: 3 }}>
-            <Paper sx={{ p: 2 }}>
-                <Typography variant="h5" gutterBottom>
-                    Add New Product
+            <Paper sx={{ p: 3 }} elevation={2}>
+                <Typography variant="h5" fontWeight="bold" gutterBottom>
+                    Add Product
                 </Typography>
-                <Typography variant="body1">
-                    Manage your products here.
-                </Typography>
+
+                <Box component="form" onSubmit={handleSubmit} noValidate autoComplete="off">
+                    <Grid container spacing={2}>
+                        {/* Category */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                select
+                                fullWidth
+                                label="Category"
+                                name="category"
+                                value={form.category}
+                                onChange={handleChange}
+                            >
+                                {categories.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </TextField>
+                        </Grid>
+
+                        {/* Sub Category */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                select
+                                fullWidth
+                                label="Sub-Category"
+                                name="subCategory"
+                                value={form.subCategory}
+                                onChange={handleChange}
+                            >
+                                {subCategories.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </TextField>
+                        </Grid>
+
+                        {/* Product Type */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                select
+                                fullWidth
+                                label="Product Type"
+                                name="productType"
+                                value={form.productType}
+                                onChange={handleChange}
+                            >
+                                {productTypes.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </TextField>
+                        </Grid>
+
+                        {/* Product Name */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                label="Product Name"
+                                name="productName"
+                                value={form.productName}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+
+                        {/* Product Code */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                label="Product Code"
+                                name="productCode"
+                                value={form.productCode}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+
+                        {/* Striked Price */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                label="Striked Price (Rs.)"
+                                name="strikePrice"
+                                value={form.strikePrice}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+
+                        {/* Display Price */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                label="Display Price (Rs.)"
+                                name="displayPrice"
+                                value={form.displayPrice}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+
+                        {/* Available in Outlet */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                label="Available in Outlet"
+                                name="availableOutlet"
+                                value={form.availableOutlet}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+
+                        {/* Product Sequence */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                label="Product Sequence"
+                                name="productSequence"
+                                value={form.productSequence}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+
+                        {/* Product Description */}
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                multiline
+                                minRows={3}
+                                label="Product Description"
+                                name="productDescription"
+                                value={form.productDescription}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+
+                        {/* Offer Available */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                select
+                                fullWidth
+                                label="Offer Available"
+                                name="offer"
+                                value={form.offer}
+                                onChange={handleChange}
+                            >
+                                {offers.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </TextField>
+                        </Grid>
+
+                        {/* Recommended */}
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                select
+                                fullWidth
+                                label="Recommended?"
+                                name="recommended"
+                                value={form.recommended}
+                                onChange={handleChange}
+                            >
+                                {recommended.map((option) => (
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                ))}
+                            </TextField>
+                        </Grid>
+
+                        {/* Add Link */}
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                label="Add Link"
+                                name="link"
+                                value={form.link}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+
+                        {/* Submit Button */}
+                        <Grid item xs={12}>
+                            <Button
+                                variant="contained"
+                                type="submit"
+                                startIcon={<CheckCircleIcon />}
+                                sx={{
+                                backgroundColor: '#fc8019',
+                                    '&:hover': {
+                                        backgroundColor: '#ff9d3b'
+                                    }
+                                }}
+                            >
+                                Submit
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Box>
             </Paper>
         </Box>
     );
-}
+};
 
 export default AddNewProduct;
